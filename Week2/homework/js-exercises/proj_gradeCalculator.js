a50eccf79c02ebc9c4f581fc2a75322317e208d0
Saeed Rafay <saeedahmadrafay@hotmail.com>
2020-11-06 14:56:32 +0100

    Added comments to the project Grade Calculator

--- Week2/homework/js-exercises/proj_gradeCalculator.js
+++ Week2/homework/js-exercises/proj_gradeCalculator.js
@@ -1,9 +1,14 @@
 "use strict";
 
-
+// the function takes two numbers as parameters
+// first parameter takes the score acheived
+// the second parameter takes the total score
 const gradeCalculator = (a, b) => {
-  //
+  // convert score into a percentage
   const percentage = (a / b) * 100;
+
+  // checking condition is each case
+  // if the condition matches then the switch breaks and the value is returned
   switch (true) {
     case percentage >= 90:
       return `You got an A (${percentage}%)`;
@@ -26,4 +31,7 @@ const gradeCalculator = (a, b) => {
   }
 };
 
+// testing the grade calculator function
+console.log(gradeCalculator(60, 100));
 console.log(gradeCalculator(69, 75));
+console.log(gradeCalculator(93, 120));
